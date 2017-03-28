b81e9570523efe493c9f15ad17db2f9985667ae9
Alex Eastaugh <alex.eastaugh@gmail.com>
2017-03-28 20:42:31 +0100

    Cleanup

--- install.sh
+++ install.sh
@@ -11,7 +11,7 @@ ARCFILE="/etc/apt/sources.list.d/arc-theme.list"
 LOG=/tmp/new_install.log
 
 # Update repos
-echo -e "\n$YELLOW Updating repos.... $CEXIT\n"
+echo -e "\n${YELLOW}Updating repos.... ${CEXIT}\n"
 sudo apt -qq update
 
 # SSH check and setup
@@ -19,9 +19,9 @@ function ssh_setup() {
     if [ -f /home/$(whoami)/.ssh/config ]; then
         sudo find /home/$(whoami)/.ssh -type d -exec chmod 700 {} \;
         sudo find /home/$(whoami)/.ssh -type f -exec chmod 600 {} \;
-        echo -e "\n$GREEN ssh keys are installed and have correct permissions\n $CEXIT"
+        echo -e "\n${GREEN}ssh keys are installed and have correct permissions\n${CEXIT}"
     else
-        echo -e "\n$RED Please copy your ssh keys and config file to the .ssh dir\n $CEXIT"
+        echo -e "\n${RED}Please copy your ssh keys and config file to the .ssh dir\n${CEXIT}"
         exit 1
     fi
 }
@@ -29,18 +29,18 @@ function ssh_setup() {
 ssh_setup
 
 # User details for .gitconfig
-read -p " Please enter your full name: " NAME
-read -p " Please enter your email address: " EMAILADDRESS
-echo -e "\n$GREEN Hello there $NAME, shall we begin....\n $CEXIT"
+read -p "Please enter your full name: " NAME
+read -p "Please enter your email address: " EMAILADDRESS
+echo -e "\n${GREEN}Hello there ${NAME}, shall we begin....\n${CEXIT}"
 sleep 2
 
 # Create projects dir
 function project_dir() {
     if [ -d /home/$(whoami)/projects ]; then
-        echo -e "$GREEN Projects dir already created $CEXIT"
+        echo -e "${GREEN}Projects dir already created${CEXIT}"
     else
-        echo -e "$YELLOW Creating projects dir $CEXIT"
-    mkdir -p /home/$USER/projects >> $LOG 2>&1
+        echo -e "${YELLOW}Creating projects dir${CEXIT}"
+    mkdir -p /home/${USER}/projects >> ${LOG} 2>&1
     fi
 }
 
@@ -49,10 +49,10 @@ project_dir
 # Install apt packages
 function install_package() {
     if dpkg-query --list | grep -m1 -q "$1"; then
-        echo -e "$GREEN $1 is already installed $CEXIT"
+        echo -e "${GREEN}$1 is already installed${CEXIT}"
     else
-        echo -e "$YELLOW Installing $1 $CEXIT"
-        sudo apt install --assume-yes "$@" >> $LOG 2>&1
+        echo -e "${YELLOW}Installing $1${CEXIT}"
+        sudo apt install --assume-yes "$@" >> ${LOG} 2>&1
     fi
 }
 
@@ -72,8 +72,8 @@ function clone_dots() {
     if [ ! -d /home/$(whoami)/dotfiles ]; then
         git clone git@github.com:$(whoami)eastaugh/dotfiles.git /home/$(whoami)/dotfiles
     fi
-    git config --global user.name "$NAME"
-    git config --global user.email $EMAILADDRESS
+    git config --global user.name "${NAME}"
+    git config --global user.email ${EMAILADDRESS}
     git config --global push.default simple
 }
 
@@ -83,7 +83,7 @@ function apply_dots() {
         cd /home/$(whoami)/dotfiles
         stow bash vim tmux mpv moc
     else
-        mv /home/$(whoami)/.bashrc /home/$(whoami)/.bashrc.backup >> $LOG 2>&1
+        mv /home/$(whoami)/.bashrc /home/$(whoami)/.bashrc.backup >> ${LOG} 2>&1
         cd /home/$(whoami)/dotfiles
         stow bash vim tmux mpv moc
     fi
@@ -94,60 +94,60 @@ apply_dots
 
 # Install Monaco fonts
 if [ -d "/usr/share/fonts/truetype/ttf-monaco" ]; then
-    echo -e "$GREEN Monaco fonts are already installed $CEXIT"
+    echo -e "${GREEN}Monaco fonts are already installed${CEXIT}"
 else
-    echo -e "$YELLOW Installing Monaco fonts $CEXIT"
+    echo -e "${YELLOW}Installing Monaco fonts${CEXIT}"
     sudo mkdir -p /usr/share/fonts/truetype/ttf-monaco; cd /usr/share/fonts/truetype/ttf-monaco/
     sudo wget http://www.gringod.com/wp-upload/software/Fonts/Monaco_Linux.ttf
     sudo mkfontdir
     cd /usr/share/fonts/truetype/
     fc-cache
-    echo -e "$GREEN Monaco fonts are now installed. System restart may be required $CEXIT"
+    echo -e "${GREEN}Monaco fonts are now installed. System restart may be required${CEXIT}"
 fi
 
 # Numix theme and icons
 if ls /etc/apt/sources.list.d/numix* > /dev/null 2>&1; then
-    echo -e "$GREEN Numix already installed $CEXIT"
+    echo -e "${GREEN}Numix already installed${CEXIT}"
 else
-    echo -e "$YELLOW Installing Numix theme and icons $CEXIT"
+    echo -e "${YELLOW}Installing Numix theme and icons${CEXIT}"
     add-apt-repository ppa:numix/ppa
     apt-get -qq update
     apt-get -qq install numix-gtk-theme numix-icon-theme-circle numix-folders numix-icon-theme-square
-    echo -e "$GREEN Numix is now installed $CEXIT"
+    echo -e "${GREEN}Numix is now installed${CEXIT}"
 fi 
 
 # Spotify
 if [ -x /usr/share/spotify/spotify ]; then
-    echo -e "$GREEN Spotify already installed $CEXIT"
+    echo -e "${GREEN}Spotify already installed${CEXIT}"
 else
-    echo -e "$YELLOW Installing Spotify $CEXIT"
+    echo -e "${YELLOW}Installing Spotify${CEXIT}"
     apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys BBEBDCB318AD50EC6865090613B00F1FD2C19886
     echo deb http://repository.spotify.com stable non-free | sudo tee /etc/apt/sources.list.d/spotify.list
     apt-get -qq update
     apt-get -qq install spotify-client
-    echo -e "$GREEN Spotify is now installed $CEXIT"
+    echo -e "${GREEN}Spotify is now installed${CEXIT}"
 fi
 
 function arc_install() {
-    if [ $RELEASE != "16.10" ]; then
-        if [ -s "$ARCFILE" ]; then
-            echo -e "$GREEN arc-theme from PPA is already installed $CEXIT"
+    if [ ${RELEASE} != "16.10" ]; then
+        if [ -s "${ARCFILE}" ]; then
+            echo -e "${GREEN}arc-theme from PPA is already installed${CEXIT}"
         else
-            echo -e "$YELLOW Installing arc-theme from PPA $CEXIT"
+            echo -e "${YELLOW}Installing arc-theme from PPA${CEXIT}"
             wget -O http://download.opensuse.org/repositories/home:Horst3180/xUbuntu_16.04/Release.key /tmp/Release.key
             sudo apt-key add - < /tmp/Release.key
             sudo sh -c "echo 'deb http://download.opensuse.org/repositories/home:/Horst3180/xUbuntu_16.04/ /' >> /etc/apt/sources.list.d/arc-theme.list"
             sudo apt-get -qq update
             sudo apt-get -qq install arc-theme
-            echo -e "$GREEN arc-theme from PPA is now installed $CEXIT"
+            echo -e "${GREEN}arc-theme from PPA is now installed${CEXIT}"
         fi
     else
-        if dpkg-query --list | grep -m1 "arc-theme"; then
-            echo -e "$GREEN arc-theme from apt source already intalled $CEXIT"
+        if dpkg-query --list | grep -m1 "arc-theme" > /dev/null; then
+            echo -e "${GREEN}arc-theme from apt source already intalled${CEXIT}"
         else
-            echo -e "$YELLOW Installing arc-theme using apt install $CEXIT"
+            echo -e "${YELLOW}Installing arc-theme using apt install${CEXIT}"
             sudo apt -qq install arc-theme
-            echo -e "$GREEN arc-theme from apt is now installed $CEXIT"
+            echo -e "${GREEN}arc-theme from apt is now installed${CEXIT}"
         fi
     fi
 }
@@ -156,15 +156,15 @@ arc_install
 
 # Etcher
 if [ -f "/etc/apt/sources.list.d/etcher.list" ]; then
-    echo -e "$GREEN Etcher already installed $CEXIT"
+    echo -e "${GREEN}Etcher already installed${CEXIT}"
 else
-    echo -e "$YELLOW Installing Etcher $CEXIT"
+    echo -e "${YELLOW}Installing Etcher${CEXIT}"
     echo "deb https://dl.bintray.com/resin-io/debian stable etcher" > /etc/apt/sources.list.d/etcher.list
     sudo apt-key adv --keyserver hkp://pgp.mit.edu:80 --recv-keys 379CE192D401AB61
     sudo apt-get update
     apt-get install etcher-electron
-    echo -e "$GREEN Etcher is now installed $CEXIT"
+    echo -e "${GREEN}Etcher is now installed${CEXIT}"
 fi
 
-echo -e "\n$GREEN $USER your laptop is now setup! $CEXIT\n\
-$YELLOW Remember to manually switch to the arc theme and select your fonts!\n $CEXIT"
+echo -e "\n${GREEN}${USER} your laptop is now setup!${CEXIT}\n\
+${YELLOW}Remember to manually switch to the arc theme and select your fonts!\n${CEXIT}"
