dc6f28fe6134bc133fd3e77a9812973a5481110b
Alex Eastaugh <alex.eastaugh@visiondirect.co.uk>
2017-12-11 15:30:17 +0000

    virtualbox ppa now available for Zesty and Artful

--- setup.sh
+++ setup.sh
@@ -209,17 +209,17 @@ fi
 #fi
 
 # Install Virtualbox # Disabled until an Artful PPA is available
-#if [ -f /usr/bin/virtualbox ]; then
-#    echo -e "${GREEN}VirtualBox is already installed${CEXIT}"
-#else
-#    echo -e "${YELLOW}Installing VirtualBox${CEXIT}"
-#    echo "deb http://download.virtualbox.org/virtualbox/debian ${RELEASE} contrib" | sudo tee --append /etc/apt/sources.list.d/virtualbox.list
-#    sudo wget -q https://www.virtualbox.org/download/oracle_vbox_2016.asc -O- | sudo apt-key add -
-#    sudo wget -q https://www.virtualbox.org/download/oracle_vbox.asc -O- | sudo apt-key add -
-#    sudo apt-get update >/dev/null 2>&1
-#    sudo apt-get install -qq virtualbox-5.1
-#    echo -e "${GREEN}VirtualBox is now installed${CEXIT}"
-#fi
+if [ -f /usr/bin/virtualbox ]; then
+    echo -e "${GREEN}VirtualBox is already installed${CEXIT}"
+else
+    echo -e "${YELLOW}Installing VirtualBox${CEXIT}"
+    echo "deb http://download.virtualbox.org/virtualbox/debian ${RELEASE} contrib" | sudo tee --append /etc/apt/sources.list.d/virtualbox.list
+    sudo wget -q https://www.virtualbox.org/download/oracle_vbox_2016.asc -O- | sudo apt-key add -
+    sudo wget -q https://www.virtualbox.org/download/oracle_vbox.asc -O- | sudo apt-key add -
+    sudo apt-get update >/dev/null 2>&1
+    sudo apt-get install -qq virtualbox-5.1
+    echo -e "${GREEN}VirtualBox is now installed${CEXIT}"
+fi
 
 # Install Atom editor
 if [ -f /usr/bin/atom ]; then
